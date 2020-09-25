2e378d22c408e100b8123f067d5c9e1d5111ffac
froemosen <kristianantonhedegaard@gmail.com>
2020-09-25 15:19:18 +0200

    hodyman er gayman

--- Controller/lib/Grove_6Axis_Accelerometer_And_Gyroscope_BMI088-master/examples/BMI088_Example/BMI088_Example.ino
+++ Controller/lib/Grove_6Axis_Accelerometer_And_Gyroscope_BMI088-master/examples/BMI088_Example/BMI088_Example.ino
@@ -1,54 +1,52 @@
-#include <Arduino.h>
+
 #include "BMI088.h"
 
 float ax = 0, ay = 0, az = 0;
 float gx = 0, gy = 0, gz = 0;
 int16_t temp = 0;
 
-void setup() {
-  Wire.begin();
-    Serial.begin(9600);
- 
-    while(!Serial);
+void setup(void) {
+    Wire.begin();
+    Serial.begin(115200);
+
+    while (!Serial);
     Serial.println("BMI088 Raw Data");
- 
-    while(1)
-    {
-        if(bmi088.isConnection())
-        {
+
+    while (1) {
+        if (bmi088.isConnection()) {
             bmi088.initialize();
             Serial.println("BMI088 is connected");
             break;
+        } else {
+            Serial.println("BMI088 is not connected");
         }
-        else Serial.println("BMI088 is not connected");
- 
+
         delay(2000);
     }
 }
 
-void loop() {
-  bmi088.getAcceleration(&ax, &ay, &az);
+void loop(void) {
+    bmi088.getAcceleration(&ax, &ay, &az);
     bmi088.getGyroscope(&gx, &gy, &gz);
     temp = bmi088.getTemperature();
- 
+
     Serial.print(ax);
     Serial.print(",");
     Serial.print(ay);
     Serial.print(",");
     Serial.print(az);
     Serial.print(",");
- 
+
     Serial.print(gx);
     Serial.print(",");
     Serial.print(gy);
     Serial.print(",");
     Serial.print(gz);
     Serial.print(",");
- 
+
     Serial.print(temp);
- 
+
     Serial.println();
- 
+
     delay(50);
-  
 }

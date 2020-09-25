2a0c47086e5602c0c635bbd53c70cc74a09813b3
froemosen <kristianantonhedegaard@gmail.com>
2020-09-25 14:23:41 +0200

    Lys kan nu blinke ud fra main.cpp

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#define onboard 13
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(onboard, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(onboard, LOW);
+  delay(500);
+  digitalWrite(onboard, HIGH);
+  delay(200);
+    digitalWrite(onboard, LOW);
+  delay(100);
+  digitalWrite(onboard, HIGH);
+  delay(200);
+  
 }

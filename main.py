0c3785376b97afeebf9469f2d0e236bca3ca1a27
emiln2002 <emilnoergaard2014@gmail.com>
2020-10-28 12:33:44 +0100

    i put serial example in main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#pip3 install pyserial
+import serial
+import sys
+
+serialPortName = "COM4"
+baudRate = 115200
+try:s
+	s = serial.Serial(serialPortName,baudRate,timeout=1)
+except:
+	print("Failed to open", serialPortName, "as a serial port.. are you doing this right?")
+	sys.exit(1)
+
+print("Shit, it worked.. waiting for serial data...")
+
+
+try:
+	while(s.is_open):
+
+		if(s.in_waiting>0):
+			rxLine=s.readline().decode("ascii").strip()
+			print("Recieved:",rxLine)
+
+			#This is where i would unpack the recieved data with the right protocol
+			#hints: 
+			# rxsplit = rxline.split()
+			# ax = float(rxsplit[0]) etc...
+except:
+	print("Something happened... did you just yank the thing out?")
